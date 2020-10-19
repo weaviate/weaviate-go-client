8f24a70fa8a856ab366c9bd6a8e72d8ef4420073
Felix Finkbeiner <4573211+fefi42@users.noreply.github.com>
2020-10-19 13:57:14 +0200

    Add getter for enpoint without id

--- test/data/data_test.go
+++ test/data/data_test.go
@@ -48,51 +48,126 @@ func TestData_integration(t *testing.T) {
 		objectA, objErrA := client.Data.ActionGetter().WithID("565da3b6-60b3-40e5-ba21-e6bfe5dbba91").Do(context.Background())
 		assert.Nil(t, objErrA)
 
-		assert.Equal(t, "Pizza", objectT.Class)
-		valuesT := objectT.Schema.(map[string]interface{})
+		assert.Equal(t, "Pizza", objectT[0].Class)
+		valuesT := objectT[0].Schema.(map[string]interface{})
 		assert.Equal(t, "Hawaii", valuesT["name"])
-		assert.Equal(t, "Soup", objectA.Class)
-		valuesA := objectA.Schema.(map[string]interface{})
+		assert.Equal(t, "Soup", objectA[0].Class)
+		valuesA := objectA[0].Schema.(map[string]interface{})
 		assert.Equal(t, "ChickenSoup", valuesA["name"])
 
-		// Clean up classes
-		errRm := client.Schema.AllDeleter().Do(context.Background())
-		assert.Nil(t, errRm)
+		cleanUpWeaviate(t, client)
 	})
 
 	t.Run("GET /actions /things", func(t *testing.T) {
-		// Not implemented to only get thigns or actions without uuid yet
+		client := createTestClient()
+		createWeaviateTestSchemaFood(t, client)
 
-		//client := createTestClient()
-		//createWeaviateTestSchemaFood(t, client)
-		//
-		//errCreate := client.Data.Creator().WithClassName("Pizza").WithSchema(map[string]string{
-		//	"name": "Margherita",
-		//	"description": "plain",
-		//}).Do(context.Background())
-		//assert.Nil(t, errCreate)
-		//errCreate = client.Data.Creator().WithClassName("Pizza").WithSchema(map[string]string{
-		//	"name": "Pepperoni",
-		//	"description": "meat",
-		//}).Do(context.Background())
-		//assert.Nil(t, errCreate)
-		//errCreate = client.Data.Creator().WithClassName("Soup").WithKind(paragons.SemanticKindActions).WithSchema(map[string]string{
-		//	"name": "Chicken",
-		//	"description": "meat",
-		//}).Do(context.Background())
-		//assert.Nil(t, errCreate)
-		//errCreate = client.Data.Creator().WithClassName("Soup").WithKind(paragons.SemanticKindActions).WithSchema(map[string]string{
-		//	"name": "Tofu",
-		//	"description": "vegetarian",
-		//}).Do(context.Background())
-		//assert.Nil(t, errCreate)
-		//
-		//time.Sleep(2.0 * time.Second)
-		//objectT, objErrT := client.Data.ThingGetter().Do(context.Background())
-		//assert.Nil(t, objErrT)
-		//objectA, objErrA := client.Data.ActionGetter().Do(context.Background())
-		//assert.Nil(t, objErrA)
+		errCreate := client.Data.Creator().WithClassName("Pizza").WithSchema(map[string]string{
+			"name": "Margherita",
+			"description": "plain",
+		}).Do(context.Background())
+		assert.Nil(t, errCreate)
+		errCreate = client.Data.Creator().WithClassName("Pizza").WithSchema(map[string]string{
+			"name": "Pepperoni",
+			"description": "meat",
+		}).Do(context.Background())
+		assert.Nil(t, errCreate)
+		errCreate = client.Data.Creator().WithClassName("Soup").WithKind(paragons.SemanticKindActions).WithSchema(map[string]string{
+			"name": "Chicken",
+			"description": "meat",
+		}).Do(context.Background())
+		assert.Nil(t, errCreate)
+		errCreate = client.Data.Creator().WithClassName("Soup").WithKind(paragons.SemanticKindActions).WithSchema(map[string]string{
+			"name": "Tofu",
+			"description": "vegetarian",
+		}).Do(context.Background())
+		assert.Nil(t, errCreate)
 
+		time.Sleep(2.0 * time.Second)
+		objectT, objErrT := client.Data.ThingGetter().Do(context.Background())
+		assert.Nil(t, objErrT)
+		objectA, objErrA := client.Data.ActionGetter().Do(context.Background())
+		assert.Nil(t, objErrA)
+
+		assert.Equal(t, 2, len(objectT))
+		assert.Equal(t, 2, len(objectA))
+		cleanUpWeaviate(t, client)
+	})
+
+	t.Run("GET underscore properties", func(t *testing.T) {
+		client := createTestClient()
+
+		createWeaviateTestSchemaFood(t, client)
+
+		propertySchemaT := map[string]string{
+			"name": "Hawaii",
+			"description": "Universally accepted to be the best pizza ever created.",
+		}
+		propertySchemaA := map[string]string{
+			"name": "ChickenSoup",
+			"description": "Used by humans when their inferior genetics are attacked by microscopic organisms.",
+		}
+		errCreateT := client.Data.Creator().WithClassName("Pizza").WithID("abefd256-8574-442b-9293-9205193737ee").WithSchema(propertySchemaT).Do(context.Background())
+		assert.Nil(t, errCreateT)
+		errCreateA := client.Data.Creator().WithClassName("Soup").WithID("565da3b6-60b3-40e5-ba21-e6bfe5dbba91").WithSchema(propertySchemaA).WithKind(paragons.SemanticKindActions).Do(context.Background())
+		assert.Nil(t, errCreateA)
+
+		time.Sleep(2.0 * time.Second) // Give weaviate time to update its index
+		objectT, objErrT := client.Data.ThingGetter().WithID("abefd256-8574-442b-9293-9205193737ee").WithUnderscoreInterpretation().Do(context.Background())
+		assert.Nil(t, objErrT)
+		objectA, objErrA := client.Data.ActionGetter().WithID("565da3b6-60b3-40e5-ba21-e6bfe5dbba91").WithUnderscoreInterpretation().Do(context.Background())
+		assert.Nil(t, objErrA)
+
+		assert.Equal(t, "Pizza", objectT[0].Class)
+		valuesT := objectT[0].Schema.(map[string]interface{})
+		assert.Equal(t, "Hawaii", valuesT["name"])
+		assert.Equal(t, "Soup", objectA[0].Class)
+		valuesA := objectA[0].Schema.(map[string]interface{})
+		assert.Equal(t, "ChickenSoup", valuesA["name"])
+
+		cleanUpWeaviate(t, client)
+
+
+		// TODO underscore properties
+		//.withUnderscoreClassification()
+		//.withUnderscoreNearestNeighbors()
+		//.withUnderscoreFeatureProjection()
+		//.withUnderscoreVector()
+
+	})
+
+
+	t.Run("PUT /{type}/{id}", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("PATCH(merge) /{type}/{id}", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("DELETE /{type}", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("POST /{type}/{id}/references/{propertyName}", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("PUT /{type}/{id}/references/{propertyName}", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("DELETE /{type}/{id}/references/{propertyName}", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("POST /{type}/validate", func(t *testing.T) {
+		t.Fail()
+	})
+
+	t.Run("Batching??", func(t *testing.T) {
+		// TODO not sure if this should be in seperate package
+		t.Fail()
 	})
 
 	t.Run("tear down weaviate", func(t *testing.T) {
@@ -102,6 +177,12 @@ func TestData_integration(t *testing.T) {
 			t.Fail()
 		}
 	})
+}
+
+func cleanUpWeaviate(t *testing.T, client *weaviateclient.WeaviateClient) {
+	// Clean up all classes and by that also all data
+	errRm := client.Schema.AllDeleter().Do(context.Background())
+	assert.Nil(t, errRm)
 }
 
 func createTestClient() *weaviateclient.WeaviateClient {
--- weaviateclient/data/getter.go
+++ weaviateclient/data/getter.go
@@ -29,33 +29,48 @@ func (getter *ThingGetter) WithID(uuid string) *ThingGetter {
 	return getter
 }
 
-func (getter *ActionGetter) Do(ctx context.Context) (*models.Action, error) {
-	path := fmt.Sprintf("/actions/%v", getter.uuid)
-	responseData, err := getter.connection.RunREST(ctx, path, http.MethodGet, nil)
+func (getter *ActionGetter) Do(ctx context.Context) ([]*models.Action, error) {
+	responseData, err := getObjectList(ctx, "/actions", getter.uuid, getter.connection)
 	if err != nil {
 		return nil, err
 	}
-	if responseData.StatusCode == 200 {
-		var action models.Action
-		decodeErr := responseData.DecodeBodyIntoTarget(&action)
-		return &action, decodeErr
+	if responseData.StatusCode != 200 {
+		return nil, clienterrors.NewUnexpectedStatusCodeErrorFromRESTResponse(responseData)
 	}
-	return nil, clienterrors.NewUnexpectedStatusCodeErrorFromRESTResponse(responseData)
+	if getter.uuid == "" {
+		var actions models.ActionsListResponse
+		decodeErr := responseData.DecodeBodyIntoTarget(&actions)
+		return actions.Actions, decodeErr
+	}
+	var action models.Action
+	decodeErr := responseData.DecodeBodyIntoTarget(&action)
+	return []*models.Action{&action}, decodeErr
 }
 
-func (getter *ThingGetter) Do(ctx context.Context) (*models.Thing, error) {
-	path := fmt.Sprintf("/things/%v", getter.uuid)
-	responseData, err := getter.connection.RunREST(ctx, path, http.MethodGet, nil)
+func (getter *ThingGetter) Do(ctx context.Context) ([]*models.Thing, error) {
+	responseData, err := getObjectList(ctx, "/things", getter.uuid, getter.connection)
 	if err != nil {
 		return nil, err
 	}
-	if responseData.StatusCode == 200 {
-		var thing models.Thing
-		decodeErr := responseData.DecodeBodyIntoTarget(&thing)
-		return &thing, decodeErr
+	if responseData.StatusCode != 200 {
+		return nil, clienterrors.NewUnexpectedStatusCodeErrorFromRESTResponse(responseData)
 	}
-	return nil, clienterrors.NewUnexpectedStatusCodeErrorFromRESTResponse(responseData)
+	if getter.uuid == "" {
+		var things models.ThingsListResponse
+		decodeErr := responseData.DecodeBodyIntoTarget(&things)
+		return things.Things, decodeErr
+	}
+	var thing models.Thing
+	decodeErr := responseData.DecodeBodyIntoTarget(&thing)
+	return []*models.Thing{&thing}, decodeErr
 }
 
-
+func getObjectList(ctx context.Context, basePath string, uuid string, con *connection.Connection) (*connection.ResponseData, error) {
+	path := basePath
+	if uuid != "" {
+		path += fmt.Sprintf("/%v", uuid)
+	}
+	responseData, err := con.RunREST(ctx, path, http.MethodGet, nil)
+	return responseData, err
+}
 
