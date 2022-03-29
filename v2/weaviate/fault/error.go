2795caab999e207f77e1b3f8dbecefd2f7e1152a
Parker Duckworth <parkerduckworth@gmail.com>
2022-03-29 08:07:45 -0500

    gofmt

--- v2/weaviate/fault/error.go
+++ v2/weaviate/fault/error.go
@@ -8,14 +8,12 @@ import "fmt"
 //  If the error occurred for another reason the DerivedFromError will be set and IsUnexpectedStatusCode will be false
 type WeaviateClientError struct {
 	IsUnexpectedStatusCode bool
-	StatusCode int
-	Msg        string
-	DerivedFromError error
+	StatusCode             int
+	Msg                    string
+	DerivedFromError       error
 }
 
 // Error message of the unexpected status code error
 func (uce *WeaviateClientError) Error() string {
 	return fmt.Sprintf("status code: %v, error: %v", uce.StatusCode, uce.Msg)
 }
-
-
