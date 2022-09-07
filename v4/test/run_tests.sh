a08df5eee2b3ad071588fe706a9b24d964ba791d
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-09-07 15:08:28 +0200

    WVT-233: removes tests script for now

--- v4/test/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#docker-compose -f test/docker-compose.yaml run --rm -d
-echo "Unit tests"
-cd ..
-go test -v ./weaviate/...
-echo "Integration tests"
-for pkg in $(go list ./... | grep 'weaviate-go-client/v4/test'); do if ! go test -v -count 1 -race "$pkg"; then echo "Test for $pkg failed" >&2; false; exit; fi; done 
-#docker-compose -f test/docker-compose.yaml down
-#docker-compose -f test/docker-compose.yaml delete
