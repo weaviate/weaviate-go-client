0b5ad3c2e390bab316279e4b61372de73833e7ac
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-10-12 11:34:48 +0100

    WVT-362: adds support for /nodes API

---
+++ v4/weaviate/misc/nodes.go
@@ -0,0 +1,26 @@
+package misc
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/semi-technologies/weaviate-go-client/v4/weaviate/connection"
+	"github.com/semi-technologies/weaviate-go-client/v4/weaviate/except"
+	"github.com/semi-technologies/weaviate/entities/models"
+)
+
+type NodesStatusGetter struct {
+	connection *connection.Connection
+}
+
+// Do get the nodes endpoint
+func (nsg *NodesStatusGetter) Do(ctx context.Context) (*models.NodesStatusResponse, error) {
+	responseData, responseErr := nsg.connection.RunREST(ctx, "/nodes", http.MethodGet, nil)
+	err := except.CheckResponseDataErrorAndStatusCode(responseData, responseErr, 200)
+	if err != nil {
+		return nil, err
+	}
+	var nodesStatus models.NodesStatusResponse
+	parseErr := responseData.DecodeBodyIntoTarget(&nodesStatus)
+	return &nodesStatus, parseErr
+}
