5f4ebdbb885df88c4b35ea3f5d6f9594963fbc80
Dirk Kulawiak <dirk@semi.technology>
2023-02-01 16:05:13 +0100

    Fix a few more paths

--- test/stop_containers.sh
+++ test/stop_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose -f v4/test/docker-compose.yml down --remove-orphans
-docker-compose -f v4/test/docker-compose-azure.yml down --remove-orphans
-docker-compose -f v4/test/docker-compose-okta-cc.yml down --remove-orphans
-docker-compose -f v4/test/docker-compose-okta-users.yml down --remove-orphans
-docker-compose -f v4/test/docker-compose-wcs.yml down --remove-orphans
+docker-compose -f test/docker-compose.yml down --remove-orphans
+docker-compose -f test/docker-compose-azure.yml down --remove-orphans
+docker-compose -f test/docker-compose-okta-cc.yml down --remove-orphans
+docker-compose -f test/docker-compose-okta-users.yml down --remove-orphans
+docker-compose -f test/docker-compose-wcs.yml down --remove-orphans
