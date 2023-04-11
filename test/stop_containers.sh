d53a7a04828fdf865d3dbac22edbb8e53050a657
Parker Duckworth <parkerduckworth@gmail.com>
2023-04-11 17:06:48 -0500

    support gql get consistency level

--- test/stop_containers.sh
+++ test/stop_containers.sh
@@ -5,3 +5,4 @@ docker-compose -f test/docker-compose-azure.yml down --remove-orphans
 docker-compose -f test/docker-compose-okta-cc.yml down --remove-orphans
 docker-compose -f test/docker-compose-okta-users.yml down --remove-orphans
 docker-compose -f test/docker-compose-wcs.yml down --remove-orphans
+docker-compose -f test/docker-compose-cluster.yml down --remove-orphans
