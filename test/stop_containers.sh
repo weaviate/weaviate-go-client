f7d5270b14e75aa562f5e9413ed029eb704a134c
Tommy Smith <tommy@weaviate.io>
2024-07-10 13:31:53 +0100

    Change to docker compose in stop script

--- test/stop_containers.sh
+++ test/stop_containers.sh
@@ -4,11 +4,11 @@ set -eou pipefail
 
 export WEAVIATE_VERSION=$1
 
-docker-compose -f test/docker-compose.yml down --remove-orphans
-docker-compose -f test/docker-compose-azure.yml down --remove-orphans
-docker-compose -f test/docker-compose-okta-cc.yml down --remove-orphans
-docker-compose -f test/docker-compose-okta-users.yml down --remove-orphans
-docker-compose -f test/docker-compose-wcs.yml down --remove-orphans
-docker-compose -f test/docker-compose-cluster.yml down --remove-orphans
+docker compose -f test/docker-compose.yml down --remove-orphans
+docker compose -f test/docker-compose-azure.yml down --remove-orphans
+docker compose -f test/docker-compose-okta-cc.yml down --remove-orphans
+docker compose -f test/docker-compose-okta-users.yml down --remove-orphans
+docker compose -f test/docker-compose-wcs.yml down --remove-orphans
+docker compose -f test/docker-compose-cluster.yml down --remove-orphans
 
 echo "All containers stopped"
