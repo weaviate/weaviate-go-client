36fdcb3f9095709a35c2e4b791043662bf031f32
Dirk Kulawiak <dirk@semi.technology>
2024-08-14 08:42:29 +0200

    Adapt batchs tress test to use concurrency

--- test/stress/mt_batching_test.go
+++ test/stress/mt_batching_test.go
@@ -3,17 +3,19 @@ package test
 import (
 	"context"
 	"fmt"
+	"sync"
 	"testing"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/stretchr/testify/require"
 	"github.com/weaviate/weaviate-go-client/v4/test/testsuit"
-	"github.com/weaviate/weaviate-go-client/v4/weaviate/filters"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/graphql"
 	"github.com/weaviate/weaviate/entities/models"
 )
 
 func TestMTBatching_stress(t *testing.T) {
-	className := "Collection"
+	className := "MtStressTestCollection"
 	client := testsuit.CreateTestClient(true)
 	ctx := context.Background()
 
@@ -21,7 +23,7 @@ func TestMTBatching_stress(t *testing.T) {
 		err := client.Schema().ClassDeleter().WithClassName(className).Do(ctx)
 		require.Nil(t, err)
 	}()
-
+	require.Nil(t, client.Schema().ClassDeleter().WithClassName(className).Do(ctx))
 	err := client.Schema().ClassCreator().WithClass(&models.Class{
 		Class: className,
 		Properties: []*models.Property{
@@ -47,61 +49,63 @@ func TestMTBatching_stress(t *testing.T) {
 	}).Do(ctx)
 	require.Nil(t, err)
 
-	t.Run("Create 1000 objects per 1000 tenants", func(t *testing.T) {
-		tenantsN := 1000
-		objectsN := 1000
-		count := 0
-		batcher := client.Batch().ObjectsBatcher()
-		for i := 0; i < tenantsN; i++ {
-			tenant := fmt.Sprintf("tenant-%v", i)
-			for j := 0; j < objectsN; j++ {
+	tenantsN := 100
+	objectsN := 25
+
+	wg := sync.WaitGroup{}
+	eg := errgroup.Group{}
+	eg.SetLimit(20)
+	for i := 0; i < objectsN; i++ {
+		i := i
+		wg.Add(1)
+		eg.Go(func() error {
+			batcher := client.Batch().ObjectsBatcher()
+
+			for j := 0; j < tenantsN; j++ {
+				tenant := fmt.Sprintf("tenant-%v", j)
 				vector := make([]float32, 16)
 				for k := range vector {
 					vector[k] = float32(i+j+k) / float32(16+i+j)
 				}
 				obj := &models.Object{
-					Class: "Collection",
+					Class: className,
 					Properties: map[string]interface{}{
-						"name":  fmt.Sprintf("object-%v", j),
-						"count": j,
-						"price": 42.0 * float64(j),
+						"name":  fmt.Sprintf("object-%v", i),
+						"count": i,
+						"price": 42.0 * float64(i),
 					},
 					Tenant: tenant,
 					Vector: vector,
 				}
 				batcher = batcher.WithObjects(obj)
-
-				if count == 1000 {
-					_, err := batcher.Do(ctx)
-					require.Nil(t, err)
-					batcher = client.Batch().ObjectsBatcher()
-					count = 0
-				} else {
-					count++
-				}
 			}
-		}
-		_, err := batcher.Do(ctx)
-		require.Nil(t, err)
-	})
-
-	t.Run("Search for objects", func(t *testing.T) {
-		for i := 0; i < 1000; i++ {
-			tenant := fmt.Sprintf("tenant-%v", i)
-			where := &filters.WhereBuilder{}
-			res, err := client.GraphQL().Get().
-				WithClassName(className).
-				WithWhere(where.WithOperator(filters.LessThan).
-					WithPath([]string{"price"}).WithValueNumber(float64(100))).
-				WithTenant(tenant).
-				WithFields(graphql.Field{Name: "name"}).
-				Do(ctx)
+			resp, err := batcher.Do(ctx)
 			require.Nil(t, err)
-			if res.Errors != nil {
-				t.Fatalf("Search objects GQL error: %+v", res.Errors[0])
+			require.NotNil(t, resp)
+			for _, r := range resp {
+				require.Nil(t, r.Result.Errors)
+				require.Equal(t, *r.Result.Status, "SUCCESS")
 			}
-			objects := res.Data["Get"].(map[string]interface{})[className].([]interface{})
-			require.Greater(t, len(objects), 0)
-		}
-	})
+			wg.Done()
+			return nil
+		})
+	}
+	wg.Wait()
+
+	tenants, err := client.Schema().TenantsGetter().WithClassName(className).Do(ctx)
+	require.Nil(t, err)
+	require.Len(t, tenants, tenantsN)
+	for i := 0; i < tenantsN; i++ {
+		tenant := fmt.Sprintf("tenant-%v", i)
+		res, err := client.GraphQL().Get().
+			WithClassName(className).
+			WithTenant(tenant).
+			WithFields(graphql.Field{Name: "name"}).
+			Do(ctx)
+		require.Nil(t, err)
+		require.Nil(t, res.Errors)
+
+		objects := res.Data["Get"].(map[string]interface{})[className].([]interface{})
+		require.Equal(t, len(objects), objectsN)
+	}
 }
