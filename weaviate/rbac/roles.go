395b28cbf18bec66c32b034808148c247d8dc669
dyma solovei <dmytro.y.solovei@gmail.com>
2025-01-30 14:20:45 +0100

    chore: gofumpt the whole project

--- weaviate/rbac/roles.go
+++ weaviate/rbac/roles.go
@@ -20,36 +20,47 @@ func New(connection *connection.Connection) *API {
 func (api *API) Creator() *RoleCreator {
 	return &RoleCreator{connection: api.connection}
 }
+
 func (api *API) Deleter() *RoleDeleter {
 	return &RoleDeleter{connection: api.connection}
 }
+
 func (api *API) PermissionAdder() *PermissionAdder {
 	return &PermissionAdder{connection: api.connection}
 }
+
 func (api *API) PermissionRemover() *PermissionRemover {
 	return &PermissionRemover{connection: api.connection}
 }
+
 func (api *API) PermissionChecker() *PermissionChecker {
 	return &PermissionChecker{connection: api.connection}
 }
+
 func (api *API) AllGetter() *RoleAllGetter {
 	return &RoleAllGetter{connection: api.connection}
 }
+
 func (api *API) Getter() *RoleGetter {
 	return &RoleGetter{connection: api.connection}
 }
+
 func (api *API) UserRolesGetter() *UserRolesGetter {
 	return &UserRolesGetter{connection: api.connection}
 }
+
 func (api *API) AssignedUsersGetter() *AssignedUsersGetter {
 	return &AssignedUsersGetter{connection: api.connection}
 }
+
 func (api *API) Exists() *RoleExists {
 	return &RoleExists{connection: api.connection, getter: api.Getter()}
 }
+
 func (api *API) Assigner() *RoleAssigner {
 	return &RoleAssigner{connection: api.connection}
 }
+
 func (api *API) Revoker() *RoleRevoker {
 	return &RoleRevoker{connection: api.connection}
 }
