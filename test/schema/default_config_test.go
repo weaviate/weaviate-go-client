99d2a4f95c711c019cc5e6b3b5962c5230255a4c
Kaviraj <kavirajkanagaraj@gmail.com>
2025-07-02 17:38:08 +0200

    fix schema tests
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- test/schema/default_config_test.go
+++ test/schema/default_config_test.go
@@ -72,6 +72,11 @@ var defaultVectorIndexConfig = map[string]interface{}{
 	"sq":                     defaultSQConfig,
 	"filterStrategy":         "sweeping",
 	"multivector":            defaultMultivectorConfig,
+	"rq": map[string]interface{}{
+		"bits":         float64(8),
+		"enabled":      false,
+		"rescoreLimit": float64(20),
+	},
 }
 
 var defaultMultivectorConfig = map[string]interface{}{
