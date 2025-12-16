b3759614bf831e1c8bb1f39579724bc4bbc0ff3e
dyma solovei <dyma@weaviate.io>
2025-12-16 11:52:08 +0100

    build: bundle protoc and update go:generate directive
    This makes builds more repeatable. Also we can skip the brew install step.
    For now we only bundle protoc for macOS, because external developers wouldn't
    normally need to update generated stubs

--- internal/api/gen/proto/generate.go
+++ internal/api/gen/proto/generate.go
@@ -1,3 +1,3 @@
 package proto
 
-//go:generate sh -c "rm -rf v1 && mkdir v1 && protoc --proto_path=../../../../api/proto --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. ../../../../api/proto/v1/*.proto"
+//go:generate sh -c "rm -rf v1 && mkdir v1 && ../../../../bin/protoc --proto_path=../../../../api/proto --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. ../../../../api/proto/v1/*.proto"
