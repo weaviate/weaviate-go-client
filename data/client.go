9b389604ae4c031081b70074b51cca0df07aaf74
dyma solovei <dyma@weaviate.io>
2025-12-17 10:24:19 +0100

    wip: insert request

--- data/client.go
+++ data/client.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/weaviate/weaviate-go-client/v6/internal"
 	"github.com/weaviate/weaviate-go-client/v6/internal/api"
@@ -20,13 +21,31 @@ type Client struct {
 	defaults  api.RequestDefaults
 }
 
-func (c *Client) Insert(context.Context, ...InsertOption) (string, error) {
-	return "", nil
+func (c *Client) Insert(ctx context.Context, options ...InsertOption) (string, error) {
+	var ir insertRequest
+	InsertOptions(options).Apply(&ir)
+
+	req := &api.InsertObjectRequest{
+		UUID:       ir.UUID,
+		Properties: ir.Properties,
+		Vectors:    ir.Vectors,
+	}
+
+	var resp api.InsertObjectResponse
+	if err := c.transport.Do(ctx, req, &resp); err != nil {
+		return "", fmt.Errorf("insert object: %w", err)
+	}
+
+	return resp.UUID, nil
 }
 
 type insertRequest struct{ types.Object[types.Properties] }
 
 type InsertOption func(*insertRequest)
+
+type InsertOptions []InsertOption
+
+func (opts InsertOptions) Apply(*insertRequest) {}
 
 func WithUUID(uuid string) InsertOption {
 	return func(r *insertRequest) {
