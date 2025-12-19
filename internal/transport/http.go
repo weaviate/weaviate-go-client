2bacdfa87debd08cb06f33b13c2cb33e0d0be409
dyma solovei <dyma@weaviate.io>
2025-12-19 14:24:37 +0100

    refactor(transport/api): define Endpoint helpers for common request patterns

--- internal/transport/http.go
+++ internal/transport/http.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+
+	"github.com/weaviate/weaviate-go-client/v6/internal/dev"
 )
 
 // Endpoint describes a REST request.
@@ -123,3 +125,92 @@ func (c *httpClient) url(req Endpoint) string {
 
 	return url.String()
 }
+
+// BaseEndpoint implements [Endpoint] methods which may return nil.
+// These values are usually optional in the request.
+// BaseEndpoint can be embeded in a another request struct to reduce boilerplate.
+//
+// Example:
+//
+//	type DeleteSongRequest struct {
+//		transport.BaseEndpoint
+//		SongID string
+//	}
+//
+//	// DeleteSongRequest implements transport.Endpoint.
+//	var _ transport.Endpoint = (*DeleteSongRequest)(nil)
+//
+//	func (r *DeleteSongRequest) Method() string { return http.MethodDelete }
+//	func (r *DeleteSongRequest) Path() string   { return "/songs/" + r.SongID }
+type BaseEndpoint struct{}
+
+func (*BaseEndpoint) Query() url.Values { return nil }
+func (*BaseEndpoint) Body() any         { return nil }
+
+// StaticEndpoint creates a new static endpoint with a method and a path.
+func StaticEndpoint(method, path string) *staticEndpoint {
+	return &staticEndpoint{method: method, path: path}
+}
+
+// staticEndpoint implements [Endpoint] for requests that has
+// neither query or path parameters, nor the body. Each request
+// to a static endpoint looks identical to all other ones, e.g. GET /live.
+// Since such request is independent of its inputs (it has none of them),
+// it can be created once and reused across the program's lifetime.
+//
+// Example:
+//
+//	var ListSongsRequest transport.Endpoint = transport.StaticEndpoint(http.MethodGet, "/songs")
+type staticEndpoint struct {
+	BaseEndpoint
+	method, path string
+}
+
+var _ Endpoint = (*staticEndpoint)(nil)
+
+func (e *staticEndpoint) Method() string { return e.method }
+func (e *staticEndpoint) Path() string   { return e.path }
+
+// IdentityEndpoint returns a function to create requests to identity endpoints.
+//
+// Example:
+//
+//	// Assign request creator func to a variable.
+//	var DeleteSongRequest = transport.IdentityEndpoint[int](http.MethodDelete, "/songs/%v")
+//
+//	// Use it to create new requests
+//	req := DeleteSongRequest(123)
+//
+// The pathFmt MUST ONLY contain a single formatting directive. Callers are free to use
+// the formatting directive most appropriate to the identity type, e.g. %s for strings,
+// but %d for numbers.
+//
+// IdentityEndpoint will panic on invalid pathFmt _before_ it returns the generator.
+//
+//	// BAD: panics because pathFmt contains 2 formatting directives.
+//	var DeleteSongsRequest = transport.IdentityEndpoint[uuid.UUID](http.MethodGet, "/albums/%v/songs/%v")
+func IdentityEndpoint[I any](method, pathFmt string) func(I) Endpoint {
+	dev.Assert(strings.Count(pathFmt, "%") == 1, "%s must have a single formatting directive", pathFmt)
+
+	return func(id I) Endpoint {
+		return &identityEndpoint[I]{
+			method:  method,
+			pathFmt: pathFmt,
+			id:      id,
+		}
+	}
+}
+
+// identityEndpoint implements [Endpoint] for endpoints that use some ID parameter
+// in the request path, e.g. 'DELETE /users/<user-id>' or 'GET /artist/<artist-id>/songs'.
+// See [IdentityEndpoint] for more details.
+type identityEndpoint[I any] struct {
+	BaseEndpoint
+	method, pathFmt string
+	id              I
+}
+
+var _ Endpoint = (*identityEndpoint[any])(nil)
+
+func (r *identityEndpoint[T]) Method() string { return r.method }
+func (r *identityEndpoint[T]) Path() string   { return fmt.Sprintf(r.pathFmt, r.id) }
