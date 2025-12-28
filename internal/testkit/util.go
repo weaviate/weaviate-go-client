7111f0d3ad30deda4f39067f457682050cff3061
dyma solovei <dyma@weaviate.io>
2025-12-28 09:54:23 +0100

    fix(testkit): set Now to a UTC timestamp
    When time is a nested field in a struct, require/assert
    packages compare them with reflect.DeepEqual and not t1.Equal(t2).
    This is messing with timestamps used in tests

--- internal/testkit/util.go
+++ internal/testkit/util.go
@@ -4,8 +4,14 @@ import "time"
 
 // Now is constant across the entire test run.
 // To make Now comparable with time parsed from a string,
-// Now is stripped from any monotonic clock reading.
-var Now = time.Now().Round(0)
+// Now is stripped from any monotonic clock reading and
+// has it's Location set to nil.
+//
+// This is only important in test where we use stretchr/testify
+// packages, which do not [compare time] correctly.
+//
+// [compare time]: https://github.com/stretchr/testify/issues/502
+var Now = time.Now().UTC()
 
 // Ptr is a helper for passing pointers to constants.
 func Ptr[T any](v T) *T { return &v }
