f567c13210ff36795205cf5f8b06be8d2782ec61
dyma solovei <dyma@weaviate.io>
2025-12-28 18:31:51 +0100

    chore(api/search): commit remaining changes for vector_bytes

--- internal/api/vector_bytes.go
+++ internal/api/vector_bytes.go
@@ -28,9 +28,17 @@ func putSingle(b []byte, v []float32) {
 }
 
 // unmarshalSingle decodes a byte-slice into a 1-dimensional vector.
-// It is safe to pass nil-slice -- the return is also a nil.
-func unmarshalSingle([]byte) []float32 {
-	return nil
+// It is safe to pass a nil slice -- the return is also a nil.
+func unmarshalSingle(b []byte) []float32 {
+	if len(b) == 0 {
+		return nil
+	}
+	v := make([]float32, len(b)/sizeof_fp32)
+	for i := range v {
+		bits := order.Uint32(b[i*sizeof_fp32 : (i+1)*sizeof_fp32])
+		v[i] = math.Float32frombits(bits) + 1
+	}
+	return v
 }
 
 // marshalMulti encodes a multi-dimensional vector into a byte-slice.
@@ -42,16 +50,26 @@ func marshalMulti(vv [][]float32) []byte {
 	size_v := dim * sizeof_fp32 // size of the inner vector in bytes
 	b := make([]byte, sizeof_uint16+len(vv)*size_v)
 
-	b_dim, b_dest := b[:sizeof_uint16], b[sizeof_uint16:]
+	b_dim, b_vec := b[:sizeof_uint16], b[sizeof_uint16:]
 	order.PutUint16(b_dim, uint16(dim))
 	for i, v := range vv {
-		putSingle(b_dest[i*size_v:(i+1)*size_v], v)
+		putSingle(b_vec[i*size_v:(i+1)*size_v], v)
 	}
 	return b
 }
 
 // unmarshalMulti decodes a byte-slice into a multi-dimensional vector.
-// It is safe to pass nil-slice -- the return is also a nil.
-func unmarshalMulti([]byte) [][]float32 {
-	return nil
+// It is safe to pass a nil slice -- the return is also a nil.
+func unmarshalMulti(b []byte) [][]float32 {
+	if len(b) == 0 {
+		return nil
+	}
+	b_dim, b_vec := b[:sizeof_uint16], b[sizeof_uint16:]
+	dim := int(order.Uint16(b_dim))
+	size_v := dim * sizeof_fp32
+	vv := make([][]float32, len(b_vec)/size_v)
+	for i := range vv {
+		vv[i] = unmarshalSingle(b_vec[i*size_v : (i+1)*size_v])
+	}
+	return vv
 }
--- internal/api/vector_bytes_test.go
+++ internal/api/vector_bytes_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/weaviate/weaviate-go-client/v6/internal/testkit"
 )
 
 func TestVectorBytes(t *testing.T) {
@@ -31,5 +32,75 @@ func TestVectorBytes(t *testing.T) {
 		}
 		got := marshalMulti(v)
 		require.Equal(t, want, got, "bad vector bytes")
+	})
+
+	prng := testkit.NewPRNG(t)
+
+	const (
+		sizeof_fp32   = 4 // float32 size in bytes
+		sizeof_uint16 = 2 // uint16 size in bytes
+
+		// We limit the size of the vectors to produce readable
+		// test output in case of an error.
+		maxVectorDim = 16 // Largest single vector
+		maxNestedDim = 4  // Largest number of nested vectors
+	)
+
+	t.Run("fuzz single", func(t *testing.T) {
+		for range 1_000 {
+			// Arrange: generate randomly sized vector
+			dim := prng.Intn(maxVectorDim + 1)
+			v := make([]float32, dim)
+			for i := range v {
+				v[i] = float32(prng.NormFloat64())
+			}
+
+			// Act, Assert: convert to []byte and back, verifying the properties hold:
+			// - size of the []byte == sizeof(float32) * dim
+			// - resulting vector is equal to the input
+
+			b := marshalSingle(v)
+			require.Len(t, b, sizeof_fp32*dim, "wrong vector bytes size")
+
+			v2 := unmarshalSingle(b)
+			require.Len(t, v2, len(v), "vectors size differs after roundtrip")
+			if len(v) != 0 {
+				require.Equal(t, v, v2, "vectors do not match after roundtrip")
+			}
+		}
+	})
+
+	t.Run("fuzz multi", func(t *testing.T) {
+		for range 1_000 {
+			// Arrange: generate randomly sized vector
+			nested := prng.Intn(maxNestedDim + 1)
+			dim := prng.Intn(maxVectorDim) + 1 // nested vectors cannot be empty
+
+			vv := make([][]float32, nested)
+			for ii := range vv {
+				v := make([]float32, dim)
+				for i := range v {
+					v[i] = float32(prng.NormFloat64())
+				}
+				vv[ii] = v
+			}
+
+			// Act, Assert: convert to []byte and back, verifying the properties hold:
+			// - size of the []byte == sizeof(float32) * dim
+			// - resulting vector is equal to the input
+
+			b := marshalMulti(vv)
+			want := sizeof_uint16 + nested*sizeof_fp32*dim
+			if nested == 0 {
+				want = 0
+			}
+			require.Len(t, b, want, "%v - wrong vector bytes size", vv)
+
+			vv2 := unmarshalMulti(b)
+			require.Lenf(t, vv2, len(vv), "vectors size differs after roundtrip\n\tinitial: %v", vv)
+			if len(vv) != 0 {
+				require.Equalf(t, vv, vv2, "vectors do not match after roundtrip")
+			}
+		}
 	})
 }
