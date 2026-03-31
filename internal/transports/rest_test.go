0eeed3d6faa58f966e6ec7ecae395d706ca1ea62
dyma solovei <dmytro.y.solovei@gmail.com>
2026-03-31 14:04:36 +0200

    feat(internal/transports): implement REST transport

--- internal/transports/rest_test.go
+++ internal/transports/rest_test.go
@@ -1,16 +1,232 @@
 package transports_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"slices"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/weaviate/weaviate-go-client/v6/internal/testkit"
 	"github.com/weaviate/weaviate-go-client/v6/internal/transports"
 )
+
+func TestREST_Do(t *testing.T) {
+	contentTypeHeader := http.Header{"Content-Type": {"application/json"}}
+	defaultHeader := http.Header{"X-Default": {"a", "b"}}
+	version := "v0"
+
+	for _, tt := range testkit.WithOnly(t, []struct {
+		testkit.Only
+
+		name string
+		req  transports.Endpoint
+
+		respBody string // Set response body to return.
+		respCode int    // Override returned status code (default: HTTP 200).
+
+		// respBody will be read into dest; leave unset.
+		// want is the expected value after deserialization.
+		dest, want any
+		err        testkit.Error
+	}{
+		{
+			name: "no payload",
+			req: &endpoint{
+				method: http.MethodGet,
+				path:   "/test",
+			},
+		},
+		{
+			name: "with query",
+			req: &endpoint{
+				method: http.MethodGet,
+				path:   "/test",
+				query:  url.Values{"limit": {"10"}, "format": {"short"}},
+			},
+		},
+		{
+			name: "with payload",
+			req: &endpoint{
+				method: http.MethodPost,
+				path:   "/test",
+				body:   testkit.Ptr(5),
+			},
+			respBody: "123",
+			want:     float64(123), // "123" is unmarshaled into float64 by default.
+		},
+		{
+			name: "with malformed payload",
+			req: &endpoint{
+				method: http.MethodPost,
+				path:   "/test",
+				body:   new(malformedBody),
+			},
+			err: testkit.ExpectError,
+		},
+		{
+			name: "malformed response body",
+			req: &endpoint{
+				method: http.MethodGet,
+				path:   "/test",
+			},
+			respBody: "{id: 123}",
+			err:      testkit.ExpectError,
+		},
+		{
+			name: "error status code",
+			req: &endpoint{
+				method: http.MethodDelete,
+				path:   "/test",
+			},
+			respCode: http.StatusPaymentRequired,
+			respBody: "Payment Required",
+			err: httpError(&transports.HTTPError{
+				Code: http.StatusPaymentRequired,
+				Body: "Payment Required",
+			}),
+		},
+	}) {
+		t.Run(tt.name, func(t *testing.T) {
+			require.NotNil(t, tt.req, "bad test case: req is nil")
+
+			// Arrange
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, tt.req.Method(), r.Method, "request method")
+				assert.Equal(t, "/"+version+tt.req.Path(), r.URL.Path, "request path")
+				assert.Equal(t, tt.req.Query().Encode(), r.URL.Query().Encode(), "query parameters")
+				assert.Subset(t, r.Header, defaultHeader, "default headers missing")
+
+				// If request has no body, the r.Body is expected to be &http.noBody{}, never nil.
+				require.NotNil(t, assert.NotNil(t, r.Body, "http.Request.Body is nil"))
+				defer r.Body.Close()
+
+				gotBody, err := io.ReadAll(r.Body)
+				if expected := tt.req.Body(); expected == nil {
+					assert.Empty(t, gotBody, "request must have no body")
+				} else {
+					if assert.NoError(t, err, "read got request body") {
+						assert.Subset(t, r.Header, contentTypeHeader, "Content-Type header missing")
+
+						wantBody, err := json.Marshal(expected)
+						if assert.NoError(t, err, "marshal want request body") {
+							assert.JSONEq(t, string(wantBody), string(gotBody), "bad request body")
+						}
+					}
+				}
+
+				code := http.StatusOK
+				if tt.respCode != 0 {
+					code = tt.respCode
+				}
+				w.WriteHeader(code)
+
+				if tt.respBody != "" {
+					io.WriteString(w, tt.respBody)
+				}
+			})
+
+			srv := httptest.NewServer(handler)
+			defer srv.Close()
+
+			url, _ := url.Parse(srv.URL)
+			port, _ := strconv.Atoi(url.Port())
+			rest := transports.NewREST(transports.RESTConfig{
+				Scheme:  "http",
+				Host:    url.Hostname(),
+				Port:    port,
+				Version: version,
+				Header:  defaultHeader,
+			})
+
+			// Act
+			err := rest.Do(t.Context(), tt.req, &tt.dest)
+
+			// Assert
+			tt.err.Require(t, err, "request error")
+			assert.Equal(t, tt.want, tt.dest, "bad response value")
+		})
+	}
+
+	t.Run("status code accepter", func(t *testing.T) {
+		// Arrange
+		dest := acceptStatus{http.StatusConflict}
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer r.Body.Close()
+			w.WriteHeader(http.StatusConflict)
+			io.WriteString(w, "Already deleted")
+		})
+
+		srv := httptest.NewServer(handler)
+		defer srv.Close()
+
+		url, _ := url.Parse(srv.URL)
+		port, _ := strconv.Atoi(url.Port())
+		rest := transports.NewREST(transports.RESTConfig{
+			Scheme:  "http",
+			Host:    url.Hostname(),
+			Port:    port,
+			Version: version,
+		})
+
+		// Act
+		err := rest.Do(t.Context(), &endpoint{}, &dest)
+
+		// Assert
+		require.NoError(t, err)
+	})
+}
+
+// endpoint implements [transports.Endpoint] for testing.
+type endpoint struct {
+	method string
+	path   string
+	query  url.Values
+	body   any
+}
+
+var _ transports.Endpoint = (*endpoint)(nil)
+
+func (e *endpoint) Method() string    { return e.method }
+func (e *endpoint) Path() string      { return e.path }
+func (e *endpoint) Query() url.Values { return e.query }
+func (e *endpoint) Body() any         { return e.body }
+
+// acceptStatus implements [transports.StatusAccepter] for testing.
+type acceptStatus []int
+
+var _ transports.StatusAccepter = (*acceptStatus)(nil)
+
+func (accept *acceptStatus) AcceptStatus(code int) bool {
+	return slices.Contains(*accept, code)
+}
+
+// malformedBody returns [testkit.ErrWhaam] when marshaled to JSON.
+type malformedBody struct{}
+
+var _ json.Marshaler = (*malformedBody)(nil)
+
+func (*malformedBody) MarshalJSON() ([]byte, error) { return nil, testkit.ErrWhaam }
+
+// HTTPError expects a [transports.HTTPError].
+func httpError(want *transports.HTTPError) testkit.Error {
+	return func(tt assert.TestingT, got error, msgAndArgs ...any) (ok bool) {
+		var httpErr *transports.HTTPError
+		if assert.ErrorAs(tt, got, &httpErr, msgAndArgs...) {
+			ok = assert.Equal(tt, want.Code, httpErr.Code, "status code") &&
+				assert.Equal(tt, want.Body, httpErr.Body, "response error body")
+		}
+		return
+	}
+}
 
 func TestBaseEndoint(t *testing.T) {
 	var endpoint transports.BaseEndpoint
