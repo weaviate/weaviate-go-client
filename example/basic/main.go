64f64462e0bd798796d87ce56c0ff7813da6e42d
dyma solovei <dmytro.y.solovei@gmail.com>
2026-04-22 16:14:43 +0200

    ci(example): extend basic example with NearVector and NearText

--- example/basic/main.go
+++ example/basic/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/weaviate/weaviate-go-client/v6"
+	"github.com/weaviate/weaviate-go-client/v6/collections"
+	"github.com/weaviate/weaviate-go-client/v6/query"
 )
 
 const (
@@ -23,20 +26,35 @@ func main() {
 
 	ctx := context.Background()
 
+	// Connect to a WCD cluster.
 	c, err := weaviate.NewWeaviateCloud(ctx, host, apiKey)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// If SampleProducts collection does not exist, create it.
+	canSearch := true
 	if ok, err := c.Collections.Exists(ctx, "SampleProducts"); err != nil {
 		log.Fatal(err)
 	} else if !ok {
-		log.Print("SampleProducts collection does not exist. Skipping example.")
-		return
+		c.Collections.Create(ctx, collections.Collection{
+			Name: "SampleProducts",
+			Properties: []collections.Property{
+				{Name: "name", DataType: collections.DataTypeText},
+				{Name: "description", DataType: collections.DataTypeText},
+				{Name: "url", DataType: collections.DataTypeText},
+			},
+		})
+
+		// Current client version does not support defining vector indices.
+		// Without one, similarity search is not possible.
+		canSearch = false
 	}
 
+	// Get a handle for SampleProducts collection.
 	products := c.Collections.Use("SampleProducts")
 
+	// Insert some objects, logging the "before" and "after" counts.
 	count, err := products.Count(ctx)
 	if err != nil {
 		log.Fatal(err)
@@ -48,7 +66,7 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("object #%d=%q", i, obj.UUID)
+		fmt.Printf("\tobject #%d=%q\n", i, obj.UUID)
 	}
 
 	count, err = products.Count(ctx)
@@ -57,4 +75,58 @@ func main() {
 	}
 
 	log.Printf("collection SampleProducts has %d objects", count)
+
+	if !canSearch {
+		log.Print("collection SampleProducts has no vector indices")
+		return
+	}
+
+	// Query some objects using NearText search.
+	nt, err := products.Query.NearText(ctx, query.NearText{
+		Concepts:      []string{"sneakers", "flipflops"},
+		MoveAway:      &query.Move{Concepts: []string{"sandals"}, Force: .34},
+		Limit:         3,
+		ReturnVectors: []string{"text2vec_weaviate"}, // the default vector name
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("NearText[sneakers, flipflops] returned %d objects:", len(nt.Objects))
+	for _, obj := range nt.Objects {
+		fmt.Println("\t- ", obj.Properties["description"])
+	}
+
+	if len(nt.Objects) == 0 {
+		return
+	}
+
+	// Fetch 3 most similar objects to the first result hit
+	target := nt.Objects[0]
+	nv, err := products.Query.NearVector(ctx, query.NearVector{
+		Target:           target.Vectors["text2vec_weaviate"], // the default vector name
+		Similarity:       query.Distance(0.56),
+		AutoLimit:        2,
+		Limit:            3,
+		ReturnProperties: []string{"name", "url"},
+		ReturnMetadata:   query.ReturnMetadata{Distance: true},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Scan results into our custom Go struct
+	type Product struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+
+	decoded := make([]query.Object[Product], len(nv.Objects))
+	if err := query.Decode(nv, &decoded); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, obj := range decoded {
+		log.Printf("[%s](%s) distance=%d", obj.Properties.Name, obj.Properties.URL, obj.Metadata.Distance)
+	}
 }
