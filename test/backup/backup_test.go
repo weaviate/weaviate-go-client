f6c037b235835872109453a80124a9638825bf69
dyma solovei <dmytro.y.solovei@gmail.com>
2026-04-27 18:07:12 +0200

    test: fix incr backup test

--- test/backup/backup_test.go
+++ test/backup/backup_test.go
@@ -79,8 +79,8 @@ func TestBackups_integration(t *testing.T) {
 			createResponse, err := client.Backup().Creator().
 				WithIncludeClassNames(className).
 				WithBackend(backend).
-				WithBackupID(backupID + "_incr").
-				WithIncrementalBaseBackupID("incr_").
+				WithBackupID("incr_" + backupID).
+				WithIncrementalBaseBackupID(backupID).
 				WithWaitForCompletion(true).
 				Do(context.Background())
 
