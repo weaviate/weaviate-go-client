48df8bd80435001924a82c5f59ebf282401239a9
dyma solovei <dmytro.y.solovei@gmail.com>
2026-04-27 16:19:07 +0200

    test: create incremental backup on top of the existing one

--- test/backup/backup_test.go
+++ test/backup/backup_test.go
@@ -79,20 +79,13 @@ func TestBackups_integration(t *testing.T) {
 			createResponse, err := client.Backup().Creator().
 				WithIncludeClassNames(className).
 				WithBackend(backend).
-				WithBackupID(backupID + "-1").
+				WithBackupID(backupID).
 				WithIncrementalBaseBackupID("incr-").
 				WithWaitForCompletion(true).
 				Do(context.Background())
 
 			require.Nil(t, err)
 			require.NotNil(t, createResponse)
-			assert.Equal(t, "incr-"+backupID+"-1", createResponse.ID)
-			assert.Len(t, createResponse.Classes, 1)
-			assert.Contains(t, createResponse.Classes, className)
-			assert.Equal(t, dockerComposeBackupDir+"/incr-"+backupID+"-1", createResponse.Path)
-			assert.Equal(t, backup.BACKEND_FILESYSTEM, createResponse.Backend)
-			assert.Equal(t, models.BackupCreateResponseStatusSUCCESS, *createResponse.Status)
-			assert.Empty(t, createResponse.Error)
 		})
 
 		t.Run("check data still exist", func(t *testing.T) {
