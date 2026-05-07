4c848d360b237dc3be38f6432b68ad974b819750
dyma solovei <dmytro.y.solovei@gmail.com>
2026-05-07 20:00:28 +0200

    chore(api): sync contracts

--- internal/api/internal/gen/rest/models.go
+++ internal/api/internal/gen/rest/models.go
@@ -1164,7 +1164,7 @@ type MultiTenancyConfig struct {
 // MultipleRef Multiple instances of references to other objects.
 type MultipleRef = []SingleRef
 
-// Namespace A cluster-level namespace used to group resources under a common administrative unit. Namespace names must start with a lowercase letter, contain only lowercase letters and digits, be 3-36 characters long, and must not be a reserved name.
+// Namespace A cluster-level namespace used to group resources under a common administrative unit. Namespace names must contain only lowercase letters, digits, and hyphens, must start and end with a letter or digit, must be 3-36 characters long, and must not be a reserved name.
 type Namespace struct {
 	// Name The unique name of the namespace.
 	Name string `json:"name,omitempty"`
