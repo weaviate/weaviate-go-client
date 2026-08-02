feee401fd7b68668ec1cbb0fb8143bfb5e336cca
dyma solovei <dmytro.y.solovei@gmail.com>
2026-08-02 18:50:13 +0200

    feat(testkit): add simple wrapper for rand.Rand

---
+++ internal/testkit/prng.go
@@ -0,0 +1,76 @@
+package testkit
+
+import (
+	"log"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// EnvSeed can be used to set a seed for this run via an environment variable.
+// This is useful for reproducing failed tests that used [PRNG].
+// The seed is printed to the error log for each failed test:
+//
+//	testkit_seed=1234567
+//
+// To use the failure in the next run simply prepend this line to the go test command:
+//
+//	testkit_seed=1234567 go test ./...
+const EnvSeed = "testkit_seed"
+
+func init() {
+	if v, ok := os.LookupEnv(EnvSeed); ok {
+		i, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			log.Fatalf("invalid %s=%s, must be in64", EnvSeed, v)
+		}
+		seed = i
+	} else {
+		seed = Now.UnixNano()
+	}
+}
+
+// seed for this test run
+var seed int64
+
+type PRNG struct {
+	t *testing.T
+
+	mu   sync.Mutex
+	rand *rand.Rand
+}
+
+// NewPRNG returns a new pseudo-random number generator.
+// By default PRNG is seeded to the current unix timestamp.
+// The seed is constant during a single test run.
+// Set a custom seed via an envvar before running the tests:
+//
+//	testkit_seed=1234567 go test ./...
+func NewPRNG(t *testing.T) *PRNG {
+	t.Logf("%s=%d", EnvSeed, seed)
+	return &PRNG{
+		t:    t,
+		rand: rand.New(rand.NewSource(seed)),
+	}
+}
+
+func (prng *PRNG) Chance(numerator, denominator int) bool {
+	prng.mu.Lock()
+	defer prng.mu.Unlock()
+
+	require.GreaterOrEqual(prng.t, numerator, 0, "numerator")
+	require.Greater(prng.t, denominator, 0, "denominator")
+	require.Less(prng.t, denominator, math.MaxInt, "denominator")
+	return prng.rand.Intn(denominator+1) < numerator
+}
+
+func (prng *PRNG) Bool() bool {
+	prng.mu.Lock()
+	defer prng.mu.Unlock()
+	return prng.Chance(1, 2)
+}
