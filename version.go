2533433a0b67baf8133de393b1d7f0ba5735141e
dyma solovei <dmytro.y.solovei@gmail.com>
2026-08-04 19:19:54 +0200

    release: update version.go to v6.0.0-beta.1

--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package weaviate
 
-var version = "v6.0.0-beta"
+var version = "v6.0.0-beta.1"
 
 // Version reports the version of the package.
 // This is sent in the X-Weaviate-Client header for telemetry.
